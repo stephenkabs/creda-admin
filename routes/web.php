cee0855c9f251d77d1efa006c4a6fe3412c1ff5c
stephenframe <167198371+stephenframe@users.noreply.github.com>
2026-02-11 02:35:40 +0200

    Pusing

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
     Route::get('/dashboard/{property?}', [AdminController::class, 'index'])
